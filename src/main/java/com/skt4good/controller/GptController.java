3aff83274efe8752446ed60bb9126c940dc580b3
wooandrich <brad1182@naver.com>
2023-09-07 22:00:02 +0900

    키워드 수정

--- src/main/java/com/skt4good/controller/GptController.java
+++ src/main/java/com/skt4good/controller/GptController.java
@@ -9,19 +9,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 @Slf4j
 @RequestMapping("/api/v1/chat-gpt")
-public class testController {
+public class GptController {
 
     private final ChatService chatService;
     private final ChatgptService chatgptService;
 
     @PostMapping("")
-    public String test(@RequestBody String question) {
-        return chatService.getChatResponse(
-                "사용자가 글을 쓰면 {진로고민, 심리, 인간관계, 건강, 취미, 취업, 실패} 중에 가장 가까운 키워드 하나를 알려줘 :"
-                + question);
+    public List<String> test(@RequestBody String question) {
+        List<String> arr = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            arr.add(chatService.getChatResponse(
+                    "사용자가 글을 쓰면 {사회생활, 미래설계, 인간관계, 건강, 실패} 중에 가장 가까운 키워드 하나를 알려줘 :"
+                            + question));
+
+        }
+
+        return arr;
     }
 }

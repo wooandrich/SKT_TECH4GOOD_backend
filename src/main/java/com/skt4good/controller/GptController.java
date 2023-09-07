cd529dca5ba82594853277bccdba614c323c5762
wooandrich <brad1182@naver.com>
2023-09-07 23:47:56 +0900

    keyword clear

--- src/main/java/com/skt4good/controller/GptController.java
+++ src/main/java/com/skt4good/controller/GptController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 @RequiredArgsConstructor
 @RestController
@@ -25,10 +26,16 @@ public class GptController {
     public List<String> test(@RequestBody String question) {
         List<String> arr = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
-            arr.add(chatService.getChatResponse(
-                    "사용자가 글을 쓰면 {사회생활, 미래설계, 인간관계, 건강, 실패} 중에 가장 가까운 키워드 하나를 알려줘 :"
-                            + question));
+            String response = chatService.getChatResponse(
+                    "사용자가 글을 쓰면 {사회생활, 미래설계, 인간관계, 건강, 실패} 중에 가장 가까운 키워드 하나를 알려줘 : "
+                            + question);
 
+            response = response.replaceAll("\n", "");
+            response = response.replaceAll(":", "");
+            response = response.replaceAll("키워드", "");
+            response = response.replaceAll("Answer", "");
+            response = response.replaceAll(" ", "");
+            arr.add(response);
         }
 
         return arr;
---
+++ src/main/java/com/skt4good/dto/QuestionDto.java
@@ -0,0 +1,17 @@
+package com.skt4good.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuestionDto {
+
+    private String question1;
+    private String question2;
+    private String question3;
+}
